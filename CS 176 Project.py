171227544d8f8953fbf0060e6ed274c67f0602b1
noeliaalvre <157081913+noeliaalvre@users.noreply.github.com>
2024-11-18 18:45:04 -0500

    Add files via upload

---
+++ CS 176 Project.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# DO NOT MODIFY THIS CELL!!!
+
+# import pandas - pd is the standard alias for Pandas
+import pandas as pd
+
+# read data from file to a dataframe
+df_census = pd.read_csv('2016-vote-fips-acp.csv',sep=',')
+df_census.head(5)
+
+
+# In[ ]:
+
+
+
+
